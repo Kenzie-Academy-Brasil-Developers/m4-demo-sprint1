261346e4ce4f98323b6ddbf548af253e4d7bd1db
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-09-20 10:21:44 -0300

    day3

--- src/logic.ts
+++ src/logic.ts
@@ -1,10 +1,9 @@
 import { Request, Response } from "express";
 import { v4 as uuidv4 } from 'uuid';
 import { userDatabase } from "./database";
+import { IUser } from "./interfaces";
 
 export const getOneUser = (req: Request, res: Response) => {
-    // req.body, req.headers, req.params, req.query
-
     const user = userDatabase.find(user => user.id === req.params.userId);
 
     return res.status(200).json(user);
@@ -15,20 +14,16 @@ export const getUsers = (req: Request, res: Response) => {
 }
 
 export const createUser = (req: Request, res: Response) => {
-    //req.body: name, email
-    //Modelando um usuário
     const newUser = { id: uuidv4(), name: req.body.name, email: req.body.email  };
-    //Adicionando esse usuário no "banco de dados"
+
     userDatabase.push(newUser);
 
     return res.status(201).json({ message: "User sucessfully created", user: newUser });
 }
 
 export const deleteUser = (req: Request, res: Response) => {
-    // req.body, req.headers, req.params, req.query
     const index = userDatabase.findIndex(user => user.id === req.params.userId);
 
-    //Exclusão com splice (removendo um item de um index específico)
     userDatabase.splice(index, 1);
 
     return res.status(200).json({ message: "User sucessfully deleted"});
@@ -39,8 +34,30 @@ export const updateUser = (req: Request, res: Response) => {
 
     const newUser = { id: req.params.userId, name: req.body.name, email: req.body.email };
 
-    //Substituição com splice (substituindo um item específico)
     userDatabase.splice(index, 1, newUser);
 
     return res.status(200).json({ message: "User updated sucessfully", user: newUser });
+}
+
+
+export const updatePartialUser = (req: Request, res: Response) => {
+    const user = userDatabase.find(user => user.id === req.params.userId);
+
+    let userBody: Partial<IUser> = {};
+
+    Object.entries(req.body).forEach((entries) => {
+        const [key, value] = entries;
+        if(key === "name" || key === "email"){
+            userBody[key] = value as string;
+        }
+    })
+
+    //Sempre vou priorizar o que estiver do lado direito, ou seja, o que está do lado direito substitui o que está do lado esquerdo
+    const newUser = { ...user, ...userBody };
+
+    const index = userDatabase.findIndex(user => user.id === req.params.userId);
+
+    userDatabase.splice(index, 1, newUser as IUser);
+
+    return res.status(200).json({ message: "User updated sucessfully", user: newUser});
 }
